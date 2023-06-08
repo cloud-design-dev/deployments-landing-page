1e161db93bb345314187dd714e070cedf4b3d019
greyhoundforty <ryantiffany@fastmail.com>
2023-06-08 11:58:50 -0500

    feat: add Dockerfile and update app.py, offerings, requirements.txt, and templates The Dockerfile is added to the project to allow for easy containerization of the application. The app.py file is updated to use the mistune library instead of markdown2 for rendering markdown content. The offerings directory is moved to the root of the project and the markdown files are updated to use the conventional commit format. The requirements.txt file is updated to include the mistune library. The templates directory is updated to remove the home-base.html file and add the home-original.html file. The home-original.html file is updated to use font-awesome icons and a new layout.
    style(home.html): refactor home page layout and classes
    feat(home.html): add new flexbox layout to home page
    The home page layout has been refactored to use a new flexbox layout, which improves the responsiveness of the page. The classes have been updated to use more descriptive names. Additionally, the font-awesome library has been updated to version 5.15.3.
    
    chore(templates): remove unused offering_detail.html and Secure-Onboard.html templates
    The offering_detail.html template was removed, and the Secure-Onboard.html template was deleted. These templates were not being used in the application and were therefore removed to reduce clutter and improve maintainability.

--- app.py
+++ app.py
@@ -1,44 +1,32 @@
 from flask import Flask, render_template
-import os
 import frontmatter
-import markdown2
+import os
+import mistune
 
 app = Flask(__name__)
+markdown = mistune.create_markdown()
 
 @app.route('/')
 def home():
-    offerings = parse_markdown_files('templates/offerings')
-    return render_template('home.html', offerings=offerings)
-
-@app.route('/<title>')
-def offering_detail(title):
-    try:
-        offering = parse_markdown_file(f'templates/offerings/{title}.md')
-    except Exception:
-        return "Offering not found", 404
-    return render_template('offering_detail.html', offering=offering)
-
-def parse_markdown_files(directory):
-    offerings = []
-    for file in os.listdir(directory):
-        if file.endswith('.md'):
-            with open(f'{directory}/{file}', 'r') as f:
+    tiles = []
+    for filename in os.listdir('offerings'):
+        if filename.endswith('.md'):
+            with open(f'offerings/{filename}') as f:
                 post = frontmatter.load(f)
-            if 'content' in post:
-                post['content'] = markdown2.markdown(post['content'])  # Convert Markdown to HTML
-            offerings.append(post)
-    return offerings
+                tiles.append({
+                    'title': post['TITLE'],
+                    'summary': post['SUMMARY'],
+                    'icon': post['ICON'],
+                    'url': filename[:-3]  # remove .md
+                })
+    return render_template('home.html', tiles=tiles)
 
-def parse_markdown_file(filepath):
-    with open(filepath, 'r') as f:
+@app.route('/<name>')
+def offering(name):
+    with open(f'offerings/{name}.md') as f:
         post = frontmatter.load(f)
-    if 'content' in post:
-        post['content'] = markdown2.markdown(post['content'])  # Convert Markdown to HTML
-    return post
+        post.content = markdown(post.content)
+        return render_template('offering.html', post=post)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
-
-
-
+    app.run(debug=True, port=3000)
